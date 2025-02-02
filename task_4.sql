ff4c7c31703f8670f1159d3c8b1254637fb06004
catediana <catherinenanyala@gmail.com>
2025-02-02 10:18:49 -0800

    modified

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,12 @@
 USE alx_book_store;
 
-SELECT * FROM books;
+-- task_4.sql
+SELECT 
+       COLUMN_NAME,
+       COLUMN_TYPE,
+       #IS_NULLABLE,
+       #COLUMN_DEFAULT,
+       #EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+  AND TABLE_NAME = 'Books';
