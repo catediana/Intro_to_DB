040f26ab37377cda12a969069a24d7d42f09b00a
catediana <catherinenanyala@gmail.com>
2025-02-02 10:39:52 -0800

    created task_5.sql file

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Active: 1738280658735@@127.0.0.1@3306
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id,customer_name,email,address)
+VALUES
+     (1,Cole Baido,cbaidoo@sandtech.como,123 Happiness Ave,);
